80467d0d3738b9608d6b05ed0fbee1601b44e865
hassan-mahmood <14besehmahmood@seecs.edu.pk>
2018-10-01 16:20:20 +0500

    Coding Trainer

--- fcn_seg.py
+++ fcn_seg.py
@@ -2,7 +2,10 @@ from FCN8.FCN8 import *
 import torch
 import torch.nn as nn
 import torch.functional as F
-
+from Buildings_Dataset.dataset import *
+from torch.utils.data import DataLoader
+from FCN8.FCN8 import *
+import torch.optim as optim
 
 #ref: https://github.com/wkentaro/pytorch-fcn/blob/master/torchfcn/trainer.py
 def cross_entropy2d(input, target, weight=None, size_average=True):
@@ -22,4 +25,28 @@ def cross_entropy2d(input, target, weight=None, size_average=True):
         loss /= mask.data.sum()
     return loss
 
-tensor1=torch.ran
+
+map_dataset=dataset('dataset')
+dataloader=DataLoader(map_dataset,batch_size=4,shuffle=True,num_workers=2)
+
+#we are just considering buildings and background so 2 classes
+no_of_classes=2
+net=FCN8(no_of_classes)
+
+optimizer=optim.SGD(net.parameters(),lr=0.01,momentum=0.9)
+
+epochs=100
+
+for epoch in epochs:
+    running_loss=0.0
+    for i_batch,batch in enumerate(dataloader):
+        image,label=batch
+
+        optimizer.zero_grad()
+        output=net(image)
+
+        loss=cross_entropy2d(output,label)
+        loss.backward()
+        print("iter {}, loss {}".format(iter, loss.data[0]))
+        optimizer.step()
+
