515f60e026e12d177896fe1b460e496767e41468
hassan-mahmood <14besehmahmood@seecs.edu.pk>
2018-10-01 16:00:37 +0500

    Adding Dataset class

--- Buildings_Dataset/dataset.py
+++ Buildings_Dataset/dataset.py
@@ -1,9 +1,17 @@
 import torch
-
+import os
 
 class dataset(torch.utils.data.Dataset):
     def __init__(self,dirpath):
-        #this dirpath will have all the images (train or test) along with their labeled masks
+        #this dirpath will have subfolders: images, labels
         self.dirpath=dirpath
-        
+        self.images=os.listdir(os.path.join(dirpath,'images'))
+        self.labels=os.listdir(os.path.join(dirpath,'labels'))
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        pass
+
 
